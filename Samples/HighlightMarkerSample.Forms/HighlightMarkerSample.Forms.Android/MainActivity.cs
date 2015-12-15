40ca0b0a329ee203c04babcdc719e6022b6207d3
thomasgalliker <thomas@bluewin.ch>
2015-12-15 11:30:56 +0100

    Add Xamarin.Forms Android sample project

---
+++ Samples/HighlightMarkerSample.Forms/HighlightMarkerSample.Forms.Android/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+namespace HighlightMarkerSample.Forms.Android
+{
+    [Activity(Label = "HighlightMarkerSample.Forms.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            this.LoadApplication(new App());
+        }
+    }
+}
+
