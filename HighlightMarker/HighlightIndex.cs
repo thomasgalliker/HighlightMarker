b06b12363baf88d3db61c24f6769f3c0921bf5e8
thomasgalliker <thomas@bluewin.ch>
2015-12-15 15:15:51 +0100

    Change HighlightIndex from class to struct

--- HighlightMarker/HighlightIndex.cs
+++ HighlightMarker/HighlightIndex.cs
@@ -1,16 +1,11 @@
+using System;
 using System.Diagnostics;
 
 namespace HighlightMarker
 {
     [DebuggerDisplay("FromIndex = {FromIndex}, Length = {Length}, IsHighlighted = {IsHighlighted}")]
-    public class HighlightIndex // TODO GATH: Could be come a struct for performance reasons
+    public struct HighlightIndex : IEquatable<HighlightIndex>
     {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="HighlightIndex" /> class.
-        /// </summary>
-        /// <param name="fromIndex">The from index.</param>
-        /// <param name="length">The length.</param>
-        /// <param name="isHighlighted">The is highlighted.</param>
         public HighlightIndex(int fromIndex, int length, bool isHighlighted)
         {
             this.FromIndex = fromIndex;
@@ -18,22 +13,46 @@ namespace HighlightMarker
             this.IsHighlighted = isHighlighted;
         }
 
-        /// <summary>
-        ///     Gets the from index.
-        /// </summary>
-        /// <value>From index.</value>
-        public int FromIndex { get; private set; }
+        public readonly int FromIndex;
 
-        /// <summary>
-        ///     Gets a value indicating whether is highlighted.
-        /// </summary>
-        /// <value><c>true</c> if this instance is highlighted; otherwise, <c>false</c>.</value>
-        public bool IsHighlighted { get; private set; }
+        public readonly bool IsHighlighted;
 
-        /// <summary>
-        ///     Gets the length.
-        /// </summary>
-        /// <value>The length.</value>
-        public int Length { get; private set; }
+        public readonly int Length;
+
+        public bool Equals(HighlightIndex other)
+        {
+            return other.FromIndex == this.FromIndex &&
+                   other.Length == this.Length &&
+                   other.IsHighlighted == this.IsHighlighted;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (!(other is HighlightIndex))
+            {
+                return false;
+            }
+
+            return this.Equals((HighlightIndex)other);
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                this.FromIndex.GetHashCode() ^
+                this.Length.GetHashCode() ^
+                this.IsHighlighted.GetHashCode();
+        }
+
+        public static bool operator ==(HighlightIndex a, HighlightIndex b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(HighlightIndex a, HighlightIndex b)
+        {
+            return !a.Equals(b);
+        }
+
     }
 }
