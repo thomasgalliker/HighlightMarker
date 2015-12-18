16362043507ba4a3bb411db418faafe82ade1e1f
thomasgalliker <thomas@bluewin.ch>
2015-12-18 23:10:11 +0100

    Add WPF library

---
+++ HighlightMarker.WPF/ColorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace HighlightMarker
+{
+    internal static class ColorHelper
+    {
+        internal static Brush GetDefaultHighlightBrush()
+        {
+            return SystemColors.HighlightBrush;
+        }
+    }
+}
