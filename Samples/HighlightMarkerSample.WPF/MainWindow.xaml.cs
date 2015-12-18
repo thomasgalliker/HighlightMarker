d3de8363a7c8394a0b83d47d92f66a7f268953d7
thomasgalliker <thomas@bluewin.ch>
2015-12-18 23:09:48 +0100

    Add WPF sample app

---
+++ Samples/HighlightMarkerSample.WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using ObservableView;
+
+namespace HighlightMarkerSample.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableView<ListItem> ListItemsView { get; private set; }
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            var listItems = new List<ListItem>
+            {
+                new ListItem { Title = "Groceries", Subtitle = "Buy bread, cheese, apples" },
+                new ListItem { Title = "Devices", Subtitle = "Buy Nexus, Galaxy, Droid" },
+                new ListItem { Title = "Toys", Subtitle = "Buy Lego" }
+            };
+
+            this.ListItemsView = new ObservableView<ListItem>(listItems);
+            this.ListItemsView.AddSearchSpecification(x => x.Title);
+            this.ListItemsView.AddSearchSpecification(x => x.Subtitle);
+
+            this.searchBox.TextChanged += this.OnSearchBoxTextChanged;
+
+            this.DataContext = this;
+        }
+
+        private void OnSearchBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.ListItemsView.Search(((TextBox)e.Source).Text);
+        }
+    }
+}
