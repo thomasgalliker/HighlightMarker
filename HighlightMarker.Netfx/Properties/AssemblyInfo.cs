184cf1528d159fbd33e1ae67df02a91c40cc109f
thomasgalliker <thomas@bluewin.ch>
2015-12-19 02:58:34 +0100

    Add Netfx library

--- HighlightMarker.Netfx/Properties/AssemblyInfo.cs
+++ HighlightMarker.Netfx/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HighlightMarker.WPF")]
+[assembly: AssemblyTitle("HighlightMarker.Netfx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HighlightMarker.WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Thomas Galliker")]
+[assembly: AssemblyProduct("HighlightMarker.Netfx")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
