d0b6cb8a9b4dc0e0d91cfecc9e6a1e294f142d9d
thomasgalliker <thomas@bluewin.ch>
2016-10-03 21:47:29 +0200

    Update AssemblyInfo with new version

--- HighlightMarker.Netfx/Properties/AssemblyInfo.cs
+++ HighlightMarker.Netfx/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3-pre1")]
+// [assembly: AssemblyVersion("1.0.34")]
+[assembly: AssemblyVersion("1.0.34")]
+[assembly: AssemblyFileVersion("1.0.34-pre1")]
