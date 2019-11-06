bad96676f2863b5231fc306e91f649f132d47807
Thomas Galliker <thomas@bluewin.ch>
2019-11-06 10:52:39 +0100

    Add platform specific code

---
+++ HighlightMarker.NetFx/ColorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace HighlightMarker
+{
+    internal static class ColorHelper
+    {
+        internal static Brush GetDefaultForegroundBrush()
+        {
+            return SystemColors.HighlightBrush;
+        }
+
+        public static Brush GetDefaultBackgroundBrush()
+        {
+            return Brushes.Transparent;
+        }
+    }
+}
