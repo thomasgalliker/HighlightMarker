43877f92c38e7bbb56e3fbc00bbbdf0e575ee0a2
Thomas Galliker <thomas@bluewin.ch>
2022-12-31 16:24:38 +0100

    Remove sample code

--- Samples/HighlightMarkerSample.UWP/MainPage.xaml.cs
+++ Samples/HighlightMarkerSample.UWP/MainPage.xaml.cs
@@ -11,29 +11,9 @@ namespace HighlightMarkerSample.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public ObservableView<Mall> ListItemsView { get; private set; }
-
         public MainPage()
         {
             this.InitializeComponent();
-
-            var listItems = MallManager.GetMalls();
-
-            this.ListItemsView = new ObservableView<Mall>(listItems);
-            this.ListItemsView.SearchSpecification.Add(x => x.Title);
-            this.ListItemsView.SearchSpecification.Add(x => x.Subtitle);
-
-            this.searchBox.Focus();
-
-
-            // In this example we use the binding Text = "{Binding ListItemsView.SearchText, Mode=TwoWay}"
-            // in order to bind the searchBox.Text to the SearchText property of the ObservableView< Mall >.
-            // Therefore, the following OnSearchBoxTextChanged event handler can be removed:
-
-            // this.searchBox.TextChanged += this.OnSearchBoxTextChanged;
-
-
-            this.DataContext = this;
         }
     }
 }
